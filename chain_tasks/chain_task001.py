e2cc3fc012b40aff87e6f362e33ff4d90a00b140
Shailesh Rudra <rudra12shailesh@gmail.com>
2017-04-15 00:16:06 +0530

    chain coroutine added

---
+++ chain_tasks/chain_task001.py
@@ -0,0 +1,31 @@
+__author__ = 'shailesh'
+
+import asyncio
+
+@asyncio.coroutine
+def create():
+    yield from asyncio.sleep(3.0)
+    print("(1) create file")
+
+@asyncio.coroutine
+def write():
+    yield from asyncio.sleep(1.0)
+    print("(2) write into file")
+
+@asyncio.coroutine
+def close():
+    print("(3) close file")
+
+@asyncio.coroutine
+def test():
+    asyncio.ensure_future(create())
+    asyncio.ensure_future(write())
+    asyncio.ensure_future(close())
+    yield from asyncio.sleep(2.0)
+    loop.stop()
+
+loop = asyncio.get_event_loop()
+asyncio.ensure_future(test())
+loop.run_forever()
+print("Pending tasks at exit: %s" % asyncio.Task.all_tasks(loop))
+loop.close()
